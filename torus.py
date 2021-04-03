eff41edb4653baf48a40eb47dedb5f59a065d880
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2021-04-02 23:33:16 -0600

    Add files via upload

---
+++ torus.py
@@ -0,0 +1,23 @@
+import plotly.figure_factory as ff
+
+import numpy as np
+from scipy.spatial import Delaunay
+
+u = np.linspace(0, 2*np.pi, 20)
+v = np.linspace(0, 2*np.pi, 20)
+u,v = np.meshgrid(u,v)
+u = u.flatten()
+v = v.flatten()
+
+x = (3 + (np.cos(v)))*np.cos(u)
+y = (3 + (np.cos(v)))*np.sin(u)
+z = np.sin(v)
+
+points2D = np.vstack([u,v]).T
+tri = Delaunay(points2D)
+simplices = tri.simplices
+
+fig = ff.create_trisurf(x=x, y=y, z=z,
+                         simplices=simplices,
+                         title="Torus", aspectratio=dict(x=1, y=1, z=0.3))
+fig.show()
