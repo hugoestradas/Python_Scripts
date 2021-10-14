c798d2e3415bcc43c977af3c8c55bee8ea0676f1
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2021-10-13 19:43:51 -0600

    Update Find_permutations_in_strings.py

--- Find_permutations_in_strings.py
+++ Find_permutations_in_strings.py
@@ -1,26 +1,23 @@
 char_lenght = 26
   
-def factorial(n) : 
-      
-    fact = 1; 
-    for i in range(2, n + 1) : 
-        fact = fact * i; 
+def factorial(n):
+  fact = 1; 
+  for i in range(2, n + 1):
+    fact = fact * i; 
     return fact 
         
-def countPerms(st) : 
+def countPerms(st): 
+  length = len(st)
+  freq = [0] * char_lenght 
       
-    length = len(st) 
-    freq = [0] * char_lenght 
-      
-    for i in range(0, length) : 
-        if (st[i] >= 'a') : 
-            freq[(ord)(st[i]) - 97] = freq[(ord)(st[i]) - 97] + 1; 
+  for i in range(0, length):
+    if (st[i] >= 'a'):
+      freq[(ord)(st[i]) - 97] = freq[(ord)(st[i]) - 97] + 1; 
+  fact = 1
+  for i in range(0, char_lenght):
+    fact = fact * factorial(freq[i]) 
     
-    fact = 1
-    for i in range(0, char_lenght) : 
-        fact = fact * factorial(freq[i]) 
-    
-    return factorial(length) / fact 
+   return factorial(length) / fact 
   
 word = "HugoEstrada"
 print (countPerms(word))
