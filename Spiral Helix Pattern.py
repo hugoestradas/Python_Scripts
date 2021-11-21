ae454d3e1feb4a2e4a1bfa5d0fc1b58a148cfe7d
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2021-11-21 01:17:20 -0600

    Update Spiral Helix Pattern.py

--- Spiral Helix Pattern.py
+++ Spiral Helix Pattern.py
@@ -1,8 +1,13 @@
 import turtle
+
 turtle.speed(10)
+
 colors = ['red', 'purple', 'blue', 'green', 'white', 'yellow']
+
 t = turtle.Pen()
+
 turtle.bgcolor('black')
+
 for x in range(360):
     t.pencolor(colors[x%6])
     t.width(x/100 + 1)
