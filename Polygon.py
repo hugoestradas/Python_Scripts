3b33d5eb81745b50d74cd787e4914c2ab24ab259
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2021-11-21 01:19:18 -0600

    Update Polygon.py

--- Polygon.py
+++ Polygon.py
@@ -1,10 +1,13 @@
 from turtle import*
+
 speed(100)
 
 def polygon(sides, side_len):
+  
   for i in range(0, sides):
     forward(side_len)
     left(360/sides)
+  
   if side_len > 0:
     left(2)
     forward(8)
