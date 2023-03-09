a9c92859f3664143fab731774d22c8f285c14dd0
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2023-03-09 07:47:22 -0600

    Add files via upload

---
+++ rmbckgrnd.py
@@ -0,0 +1,10 @@
+from rembg import remove
+
+input_path = 'input.png'
+output_path = 'output.png'
+
+with open(input_path, 'rb') as i:
+    with open(output_path, 'wb') as o:
+        input = i.read()
+        output = remove(input)
+        o.write(output)
