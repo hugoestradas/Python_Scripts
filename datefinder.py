7a205a097e9db90754c9e27beba0c83268cd61d5
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2023-03-09 07:46:55 -0600

    Add files via upload

---
+++ datefinder.py
@@ -0,0 +1,10 @@
+from datefinder import find_dates
+
+text = """We have one meeting on Feb 17th, 2022 at 10:00am and another
+meeting on 2/22/2022 at 1:00pm."""
+
+matches = find_dates(text)
+
+for match in matches:
+    print("Date & hour: ", match)
+    print("Only day: ", match.day)
