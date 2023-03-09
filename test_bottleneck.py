eb706598ece531e8155e250aa411e01e11734613
Hugo Estrada S <61108175+hugoestradas@users.noreply.github.com>
2023-03-09 07:47:44 -0600

    Add files via upload

---
+++ test_bottleneck.py
@@ -0,0 +1,13 @@
+import bottleneck as bn
+import numpy as np
+
+"""arr = np.array([1, 2, np.nan, 4, 5, 6, 7 , 17])
+
+bn.nansum(arr)
+bn.nanmean(arr)
+bn.nanstd(arr)
+bn.nanmedian(arr)
+bn.nanmax(arr)
+"""
+
+bn.bench()
